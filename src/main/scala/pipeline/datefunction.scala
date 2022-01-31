541804f7a22bb0692a940a15e1e5a5b4b21ce71c
hduser <santosh.nzb003@gmail.com>
2022-02-01 01:23:41 +0530

    with date patterns

--- src/main/scala/pipeline/datefunction.scala
+++ src/main/scala/pipeline/datefunction.scala
@@ -3,16 +3,35 @@ package pipeline
 
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.udf
-//import scala.util.matching.Regex
+import scala.util.matching.Regex
 import java.text.SimpleDateFormat
 
 object datefunction extends idate {
 
   def dtcnv(dt_String:String): java.sql.Date={
+    val datetimePattern: Regex = "^(\\d{4})-(\\d{2})-(\\d{2})[ T]((\\d{2}):(\\d{2}):(\\d{2}))$".r
+    val datePatternYYYYMMDD: Regex = "^\\d{4}-\\d{2}-\\d{2}$".r
+    val datePatternDDMMYYYY: Regex = "^\\d{2}-\\d{2}-\\d{4}$".r
+    //val dt = new SimpleDateFormat("dd-mm-yyyy")
+    //new java.sql.Date(dt.parse(dt_String).getTime)
 
 
-    val dt = new SimpleDateFormat("dd-mm-yyyy")
-     new java.sql.Date(dt.parse(dt_String).getTime)
+    val dt_c: java.sql.Date = dt_String.trim match {
+      case datePatternYYYYMMDD() => {
+        val dt = new SimpleDateFormat("yyyy-MM-dd")
+        new java.sql.Date(dt.parse(dt_String).getTime)
+      }
+      case datePatternDDMMYYYY() =>{
+        val dt = new SimpleDateFormat("dd-MM-yyyy")
+        new java.sql.Date(dt.parse(dt_String).getTime)
+      }
+      case datetimePattern() => {
+        val dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        new java.sql.Date(dt.parse(dt_String).getTime)
+      }
+      case _ => throw new IllegalArgumentException("Wrong date/datetime format")
+    }
+    dt_c
 
   }
   val dcnv: UserDefinedFunction = {
